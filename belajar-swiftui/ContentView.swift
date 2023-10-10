850bfa7f594ff1c09190c416a4ee34ff733db202
lukmansp <lukman.sefriyanto@misteraladin.com>
2023-10-10 14:20:05 +0700

    Initial Commit

---
+++ belajar-swiftui/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  belajar-swiftui
+//
+//  Created by lukmanma on 10/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ belajar-swiftui/belajar_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  belajar_swiftuiApp.swift
+//  belajar-swiftui
+//
+//  Created by lukmanma on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct belajar_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
